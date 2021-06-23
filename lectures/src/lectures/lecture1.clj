a09d8cfe0e17a59805ba105ea6aade9c485cc129
notblank <zert.federico@gmail.com>
2021-06-23 15:09:26 -0500

    line plots done

--- lectures/src/lectures/lecture1.clj
+++ lectures/src/lectures/lecture1.clj
@@ -23,7 +23,6 @@
        (take 10000 
              (drop 1000 (doquery :lmh lin-mod [])))))
 
-
 (def scatter-plot
   {:data {:values samples-coefs}
    :encoding {:x {:field "a0" :type "quantitative"}
@@ -32,11 +31,9 @@
 
 ;; sampling a linear function:
 (defquery lin-fun []
-  (let [F (fn [] 
-            (let [a0 (sample (normal 0 6))
-                  a1 (sample (normal 0 2))]
-                  f (fn [x] (+ (* a1 x) a0))))
-            f (F)]
+  (let [a0 (sample (normal 0 6))
+        a1 (sample (normal 0 2))
+        f (fn [x] (+ (* a1 x) a0))]
     (observe (normal (f 0) 0.5) 0.6)
     (observe (normal (f 1) 0.5) 0.7)
     (observe (normal (f 2) 0.5) 1.2)
@@ -48,9 +45,8 @@
 
 (def lazy-sample-funs 
   (map :result 
-       (take 500 
+       (take 100
              (take-nth 20 (drop 1000 (doquery :lmh lin-fun []))))))
-
 
 ;; Honseok Homework 1:
 (defn uncps [f]
@@ -66,27 +62,29 @@
 (defn make-map [k x] 
   (map (fn [d] (assoc {} k d)) x))
 
+(def xrange (range -2 10 0.5))
+
 (defn xy [f]
   (map merge 
-       (make-map :x (range -2 10 0.5)) 
-       (make-map :y (map f (range -2 10 0.5)))))
+       (make-map :x xrange) 
+       (make-map :y (map f xrange))))
 
 (def y=fx (map xy sample-funs))
 
 ;; add f number:
-(map merge (first y=fx) (make-map :function (repeat 24 0)))
+(def y=fx-plot
+  (flatten
+    (for [i (range (count y=fx))]
+      (map merge (nth y=fx i) (make-map :f (repeat (count xrange) i))))))
 
-(defn play-data [& names]
-  (for [n names
-        i (range 2)]
-    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
-
-(play-data "maa" "rrr")
 
 (def line-plot
-  {:data {:values ff}
+  {:data {:values y=fx-plot}
    :encoding {:x {:field "x" :type "quantitative"}
-              :y {:field "y" :type "quantitative"}}
+              :y {:field "y" :type "quantitative"}
+              :color {:field "f" 
+                      :type "nominal"
+                      :legend false}}
    :mark "line"})
 
 (def viz
